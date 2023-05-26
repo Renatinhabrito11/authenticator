f9101aa60302bb84123563dfe2bdb219a1c9c73d
BrunoSS <simoes.bruno10@gmail.com>
2023-05-25 23:50:05 -0300

    aaaa

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;
  
 if(!is_null($captcha)){
-	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdFYiEmAAAAACRpGtr1qmjaysyATvEDzNVCjKj3&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
+	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcH5j4mAAAAABLqX8Yv1miGGfWUEQnYsncuV_M2&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
 	if($res->success === true){
 		//CAPTCHA validado!!!
 		echo 'Tudo certo =)';
